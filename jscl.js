ead4a4ede64207c7df181be0572badaef608f649
davazp <davazp@gmail.com>
2021-05-09 21:25:00 +0000

    Deploying to master from @ jscl-project/jscl@aaa2365ea55254eb25fc07c5209b89f9fb1c478a 🚀

--- jscl.js
+++ jscl.js
@@ -30065,7 +30065,7 @@ return FUNC;
 })();
 l2343;
 var l2345=internals.intern('LISP-IMPLEMENTATION-VERSION','COMMON-LISP');
-var l2346=internals.make_lisp_string('53c3aca');
+var l2346=internals.make_lisp_string('aaa2365');
 l2345.fvalue=(function(){var FUNC=(function JSCL_USER_LISPIMPLEMENTATIONVERSION(values){internals.checkArgsAtMost(arguments.length-1,0);
 var v9746=this;
 return (function(){return l2346;
